3d3897dce22023180495b2aeff70002df3e7a230
EvanDosalen <eldosalen@carc.pshs.edu.ph>
2026-09-24 16:30:39 +0800

    Add files via upload

---
+++ Dosalen_DistanceProgram.py
@@ -0,0 +1,19 @@
+#Author: Evan Jane L. Dosalen
+#Purpose: To calculate the distance between two points.
+#Date: September 24, 2026
+
+import math
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt(math.pow(x2-x1,2)+math.pow(y2-y1,2))
+
+print(f"The distance between the two points is: {distance:.2f}")
+
+#Reflection:
+#This activity helped me because I didn't have to create my own  code to calculate.
+
+
